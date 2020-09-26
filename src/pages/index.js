d95b3ec426180d5dc1a1f1b38d296500185fbd42
Karan Rajpal <rajpal.karan73@gmail.com>
2020-09-26 12:48:40 -0500

    Imported Bulma to Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello Everyone!</h1>
+    <h1 className="title">Hello Everyone!</h1>
   </Layout>
 )
 
